df8ed8730f855a4396d06dd74248f3dbcf5b3602
detrix42 <detrix42@novasector.net>
2025-07-20 01:56:55 -0400

    controller now setup and working as expected

--- app/javascript/controllers/circles_controller.js
+++ app/javascript/controllers/circles_controller.js
@@ -37,7 +37,7 @@ export default class extends Controller {
 
     if (this.circleRoleValue === 'editor') {
       this.element.innerHTML = this.circleNameValue
-      this.element.addEventListener('circle-selection:circleSelected', this.handleCircleSelection)
+      window.addEventListener('circle-selection:circleSelected', this.handleCircleSelection)
     }
 
     if (this.circleRoleValue === 'display') {
@@ -52,27 +52,24 @@ export default class extends Controller {
   }
 
   disconnect() {
+    if (this.circleRoleValue === 'editor') {
+      window.removeEventListener('circle-selection:circleSelected', this.handleCircleSelection)
+    }
+
+
     if (this.circleRoleValue === 'display') {
       this.element.removeEventListener('circle-selection:circleSelected', this.handleCircleSelection)
     }
   }
 
   handleCircleSelection = (event) => {
-
+    console.log('handle circle selection', event.detail )
     if (this.circleRoleValue === 'selector') {
-      // Remove 'selected' class from currently selected circle
-      const currentlySelected = this.element.querySelector('.selected')
-      if (currentlySelected) {
-        currentlySelected.classList.remove('selected')
-      }
-
-      // highlight the selected (active) circle
-      const tgt = this.element.querySelector(`#circle-id-${this.selectedCircleIdValue}`)
-      tgt.classList.add('selected')
-
-      // send event so another circle controller can react.
-      this.dispatchCircleEvent(this.circleNameValue)
+      // Remove the dispatchCircleEvent call, just handle the selection
+      this.changeSelectedCircle(this.selectedCircleIdValue, this)
     }
+
+
 
     if (this.circleRoleValue === 'display') {
       const tgt = this.element.querySelector(`#editor-circle-name`)
@@ -89,20 +86,20 @@ export default class extends Controller {
 
   // Action: Called on click in the selector's <li>
   select(event) {
-    // Only run if this is the selector instance
-    if (this.roleValue !== "selector") return;
+    const circleId = event.currentTarget.dataset.circlesCircleId;
+    const circleName = event.currentTarget.dataset.circlesCircleName;
+    this.changeSelectedCircle(circleId, this)
 
-    // Get circle_id from the clicked element
-    const circleId = event.currentTarget.dataset.circleId;
-    console.log(`Selected circle_id: ${circleId}`);
-
-    // Create and dispatch a custom event with circle_id in detail
-    const customEvent = new CustomEvent("circle:selected", {
-      detail: { circleId: circleId },
-      bubbles: true  // Allows bubbling if needed, but we're using window
+    const customEvent = new CustomEvent("circle-selection:circleSelected", {
+      detail: {
+        circleName: circleName,
+        circleId: circleId  // Fixed variable name
+      },
+      bubbles: true
     });
-    window.dispatchEvent(customEvent);  // Global dispatch
+    window.dispatchEvent(customEvent);
   }
+
 
   dispatchCircleEvent(circleName) {
     const circEvent = new CustomEvent("circle-selection:circleSelected", {
@@ -111,5 +108,17 @@ export default class extends Controller {
     });
     window.dispatchEvent(circEvent)
   }
+
+  changeSelectedCircle(circleID, tgtElement) {
+      console.log('change selected circle ID:', circleID)
+      const currentlySelected = this.element.querySelector('.selected')
+      if (currentlySelected) {
+        currentlySelected.classList.remove('selected')
+      }
+
+    const tgt = tgtElement.element.querySelector(`#circle-id-${circleID}`)
+    tgt.classList.add('selected')
+
+   }
 
 }
