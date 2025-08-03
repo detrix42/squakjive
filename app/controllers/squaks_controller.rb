545a708bf0800edfd64d009901672cd824481af6
detrix42 <detrix42@novasector.net>
2025-08-03 02:27:44 -0400

    this code moved to the squak model

--- app/controllers/squaks_controller.rb
+++ app/controllers/squaks_controller.rb
@@ -41,22 +41,6 @@ class SquaksController < ApplicationController
     end
   end
 
-  # def for_circle
-  #   circle_id = params[:circle_id]&.to_i
-  #   circle = circle_id == 0 ? nil : current_user.circles.find_by(id: circle_id) ||
-  #     current_user.circle_memberships.find_by(circle_id: circle_id)&.circle
-  #   squaks = Squak.for_circle(circle)
-  #   respond_to do |format|
-  #     format.turbo_stream do
-  #       render turbo_stream: turbo_stream.replace("squak-view",
-  #                                                 partial: "squaks/squak_index",
-  #                                                 locals: { squaks: squaks })
-  #     end
-  #     format.html { render partial: "squaks/squak_index", locals: { squaks: squaks } }
-  #   end
-  # end
-
-
   private
 
   def param_circle_id
