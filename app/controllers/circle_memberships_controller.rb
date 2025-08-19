11fcdf45755486e4f47bbd963b2812b3ea90c2b7
detrix42 <detrix42@novasector.net>
2025-08-19 02:47:55 -0400

    Add member management enhancements and owner indicators for circles
    Updated `CirclesHelper` and views to display owner badges, member counts, and improve member management logic. Enhanced styling for the owner and member-related indicators and applied stricter validations in `CircleMembershipsController`.

--- app/controllers/circle_memberships_controller.rb
+++ app/controllers/circle_memberships_controller.rb
@@ -3,19 +3,25 @@ class CircleMembershipsController < ApplicationController
   def destroy
     _params = circle_membership_params
 
-    @circle = current_user.circles.find(_params[:circle_id])
-    @membership = @circle.circle_memberships.find_by(user_id: _params[:user_id])
-    @mem_user = @membership.user
+    circle = current_user.circles.find(_params[:circle_id])
+    return head :not_found unless circle
+    return head :forbidden unless circle.user_id == current_user.id
 
-    @membership.destroy
+    # Do not allow removing the owner
+    return head :unprocessable_entity if _params[:user_id].to_i == circle.user_id
+
+    membership = circle.circle_memberships.find_by(user_id: _params[:user_id])
+    mem_user = membership.user
+
+    membership.destroy
 
     respond_to do |format|
       format.html { redirect_to dashboard_path }
       format.turbo_stream do
         render turbo_stream: [
-               turbo_stream.remove("sidebar-user-item-#{@mem_user.id}"),
-               turbo_stream.update("circle-members-count-#{@circle.id}",
-                                   "(#{@circle.members_count})")
+               turbo_stream.remove("sidebar-user-item-#{mem_user.id}"),
+               turbo_stream.update("circle-members-count-#{circle.id}",
+                                   "(#{circle.members_count})")
                ]
       end
     end
@@ -25,8 +31,8 @@ class CircleMembershipsController < ApplicationController
   private
 
   def circle_membership_params
-    # params.expect(:circle_membership, [[:circle_id, :user_id]])
-    params.require(:circle_membership).permit(:circle_id, :user_id)
+    params.expect(circle_membership: [:circle_id, :user_id])
+    # params.require(:circle_membership).permit(:circle_id, :user_id)
 
   end
 end
--- app/helpers/circles_helper.rb
+++ app/helpers/circles_helper.rb
@@ -1,2 +1,22 @@
 module CirclesHelper
+  def owner_and_member?(user, circle)
+    membership = circle.circle_memberships.find_by(user_id: user.id)
+    membership.present? && current_user.id == circle.user_id && user.id != circle.user_id
+
+  end
+
+  def circle_owner(circle)
+    owner = User.find_by(id: circle.user_id)
+    owner&.username || "Unknown"
+  end
+
+  def circle_owner?(circle, user)
+    cuid = circle.user_id
+    uid = user.id
+    circle.user_id == user.id
+  end
+
+  def member_count(circle)
+    circle.members.count
+  end
 end
